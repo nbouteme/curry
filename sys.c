f0e08b3f968422aa892ed30b2c31c1e1caf7c982
Nabil BOUTEMEUR <nbouteme@e3r13p2.42.fr>
2016-09-16 22:59:53 +0200

    OSX compat

--- sys.c
+++ sys.c
@@ -1,15 +1,18 @@
 void my_syscall()
 {
-	__asm__ ("movq %rdi, %rax\n\t"
-			 "movq %rsi, %rdi\n\t"
-			 "movq %rdx, %rsi\n\t"
-			 "movq %rcx, %rdx\n\t"
-			 "movq %r8, %r10\n\t"
-			 "movq %r9, %r8\n\t");
-#ifdef __OPTIMIZE__
+#ifdef __APPLE__
+	__asm__("addq $0x2000000, %rdi");
+#endif
+	__asm__("movq %rdi, %rax\n\t"
+			"movq %rsi, %rdi\n\t"
+			"movq %rdx, %rsi\n\t"
+			"movq %rcx, %rdx\n\t"
+			"movq %r8, %r10\n\t"
+			"movq %r9, %r8\n\t");
+#if defined __OPTIMIZE__ && !defined(__APPLE__)
 	__asm__("movq 0x8(%rsp), %r9");
 #else
-	__asm__ ("movq 0x10(%rbp), %r9");
+	__asm__("movq 0x10(%rbp), %r9");
 #endif
-	__asm__ ("syscall");
+	__asm__("syscall");
 }
